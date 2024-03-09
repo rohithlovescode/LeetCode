ced72aff711f8ebb20fe11fc3e623631b3f4ac51
Rohith Kumar <pathinarohith@gmail.com>
2024-03-10 02:12:13 +0530

    Time: 16 ms (63.62%) | Memory: 16.8 MB (74.90%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -1,25 +1,46 @@
-class Solution {
- public:
-  ListNode* mergeKLists(vector<ListNode*>& lists) {
-    ListNode dummy(0);
-    ListNode* curr = &dummy;
-    auto compare = [](ListNode* a, ListNode* b) { return a->val > b->val; };
-    priority_queue<ListNode*, vector<ListNode*>, decltype(compare)> minHeap(
-        compare);
-
-    for (ListNode* list : lists)
-      if (list != nullptr)
-        minHeap.push(list);
-
-    while (!minHeap.empty()) {
-      ListNode* minNode = minHeap.top();
-      minHeap.pop();
-      if (minNode->next)
-        minHeap.push(minNode->next);
-      curr->next = minNode;
-      curr = curr->next;
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class compare{
+public:
+    bool operator()(ListNode* a, ListNode* b){
+        return a->val>b->val;
     }
+};
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        ListNode* dummy=new ListNode(0);
+        ListNode* curr=dummy;
 
-    return dummy.next;
-  }
+        priority_queue<ListNode*, vector<ListNode*>,compare> minHeap;
+        
+        int n=lists.size();
+
+        for(int i=0;i<n;i++){
+            if(lists[i]!=NULL){
+                minHeap.push(lists[i]);
+            }
+        }
+
+        while(!minHeap.empty()){
+            ListNode* topp=minHeap.top();
+            curr->next=topp;
+            curr=curr->next;
+            minHeap.pop();
+
+            if(topp&&topp->next){
+                minHeap.push(topp->next);
+            }
+        }
+        return dummy->next;
+
+    }
 };
