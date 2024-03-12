5831cb1ee0646c57dc293787c38056b9efc6a1e1
Rohith Kumar <pathinarohith@gmail.com>
2024-03-13 00:40:05 +0530

    Time: 40 ms (20.42%) | Memory: 25.8 MB (14.34%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,31 +1,30 @@
 class Solution {
-public:
-    // Function to find the length of the longest common subsequence in two strings.
-    int longestCommonSubsequence(string text1, string text2) {
-        int text1Length = text1.size(), text2Length = text2.size();
-        // Create a 2D array to store lengths of common subsequence at each index.
-        int dp[text1Length + 1][text2Length + 1];
-      
-        // Initialize the 2D array with zero.
-        memset(dp, 0, sizeof dp);
-      
-        // Loop through both strings and fill the dp array.
-        for (int i = 1; i <= text1Length; ++i) {
-            for (int j = 1; j <= text2Length; ++j) {
-                // If current characters match, add 1 to the length of the sequence
-                // until the previous character from both strings.
-                if (text1[i - 1] == text2[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-                } else {
-                    // If current characters do not match, take the maximum length
-                    // achieved by either skipping the current character of text1 or text2.
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-                }
-            }
+    int solve(string& text1 , string& text2 , vector<vector<int>>& dp,int ind1,int ind2){
+        if(ind1==-1||ind2==-1){
+            return 0;
         }
-      
-        // Return the value in the bottom-right cell which contains the
-        // length of the longest common subsequence for the entire strings.
-        return dp[text1Length][text2Length];
+        if(dp[ind1][ind2]!=-1){
+            return dp[ind1][ind2];
+        }
+        else if(text1[ind1]==text2[ind2]){
+            dp[ind1][ind2]= 1+solve(text1,text2,dp,ind1-1,ind2-1);
+            
+            return dp[ind1][ind2];
+        }
+        else{//text1[leng1-1]!=text2[leng2-1]
+            dp[ind1][ind2]=max(solve(text1,text2,dp,ind1-1,ind2),solve(text1,text2,dp,ind1,ind2-1));
+            
+            return dp[ind1][ind2];
+        }
+        
+
+    }
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int ind1=text1.length()-1;
+        int ind2=text2.length()-1;
+
+        vector<vector<int>> dp(ind1+1,vector<int> (ind2+1,-1));
+        return solve(text1,text2,dp,ind1,ind2);
     }
 };
