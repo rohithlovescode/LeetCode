fec6948c21e2f01eb593a234a5ad8900e505fdf8
Rohith Kumar <pathinarohith@gmail.com>
2024-08-05 18:14:15 +0530

    Time: 6 ms (28.79%) | Memory: 11.8 MB (65.89%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -25,7 +25,9 @@ public:
         int c=costs[2]+solve(next+30,days,costs,dp);
 
         int sol= min(a,min(b,c));
-        dp[next]=sol;
+        for(int i=solvedUntil;i<=next;i++){
+            dp[i]=sol;
+        }
         return dp[next];
     }
     int mincostTickets(vector<int>& days, vector<int>& costs) {
