78302caecf956fa135241a0698cbd2c9dbc4fdb3
Rohith Kumar <pathinarohith@gmail.com>
2024-08-16 12:46:02 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (38.09%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,51 +1,59 @@
 class Solution {
 public:
-    bool isSafe(int i,int j,vector<string> curr,int n){
-        for(int a=0;a<n;a++){
-            if(curr[a][j]=='Q'){
-                return false;
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> results;
+        if (n == 1) {
+            results.push_back({"Q"});
+            return results;
+        }
+        if (n == 2 || n == 3) {
+            return results;
+        }
+
+        vector<int> solution(n, -1);
+        solveNQueensRec(n, solution, 0, results);
+        return results;
+    }
+
+private:
+    void solveNQueensRec(int n, vector<int>& solution, int row,
+                         vector<vector<string>>& results) {
+        if (row == n) {
+            vector<string> solutionStr = constructSolutionString(solution);
+            results.push_back(solutionStr);
+            return;
+        }
+
+        for (int col = 0; col < n; col++) {
+            if (isValidMove(row, col, solution)) {
+                solution[row] = col;
+                solveNQueensRec(n, solution, row + 1, results);
+                solution[row] = -1; // Backtrack
             }
         }
-        for(int b=0;b<n;b++){
-            if(curr[i][b]=='Q'){
-                return false;
-            }
-        }
-        for(int a=i,b=j;a>=0&&b>=0;a--,b--){
-            if(curr[a][b]=='Q'){
-                return false;
-            }
-        }
-        for(int a=i,b=j;a<n&&b>=0;a++,b--){
-            if(curr[a][b]=='Q'){
+    }
+
+    bool isValidMove(int proposedRow, int proposedCol,
+                     const vector<int>& solution) {
+        for (int i = 0; i < proposedRow; i++) {
+            int oldRow = i;
+            int oldCol = solution[i];
+            int diagonalOffset = proposedRow - oldRow;
+
+            if (oldCol == proposedCol ||
+                oldCol == proposedCol - diagonalOffset ||
+                oldCol == proposedCol + diagonalOffset) {
                 return false;
             }
         }
         return true;
     }
-    void solve(int n, int col,vector<string>& curr,vector<vector<string>>& sol){
-        if(col>=n){
-            sol.push_back(curr);
-            return;
-        }
-        for(int i=0;i<n;i++){
-            if(isSafe(i,col,curr,n)){
-                curr[i][col]='Q';
-                solve(n,col+1,curr,sol);
-                curr[i][col]='.';
-            }
-        }
-    }
-    vector<vector<string>> solveNQueens(int n) {
-        vector<string> temp;
-        string nullString(n,'.');
-        
-        vector<string> curr(n,nullString);
-        vector<vector<string>> sol;
 
-        int col=0;
-        solve(n,col,curr,sol);
-
-        return sol;
+    vector<string> constructSolutionString(const vector<int>& solution) {
+        vector<string> returnArr(solution.size(), string(solution.size(), '.'));
+        for (int i = 0; i < solution.size(); i++) {
+            returnArr[i][solution[i]] = 'Q';
+        }
+        return returnArr;
     }
 };
