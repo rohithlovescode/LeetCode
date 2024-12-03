2972d87e9567dbc6156680109646b2322bebf791
Rohith Kumar <pathinarohith@gmail.com>
2024-12-04 00:29:29 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (5.71%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
-    int uniquePaths(int m, int n) {
-        std::vector<int> aboveRow(n, 1);
-
-        for (int row = 1; row < m; row++) {
-            std::vector<int> currentRow(n, 1);
-            for (int col = 1; col < n; col++) {
-                currentRow[col] = currentRow[col - 1] + aboveRow[col];
-            }
-            aboveRow = currentRow;
+    int recurse(int i,int j,int m,int n,vector<vector<int>>& dp){
+        if(i>=m||j>=n){
+            return 0;
         }
-
-        return aboveRow[n - 1];        
+        if(dp[i][j]!=0){
+            return dp[i][j];
+        }
+        dp[i][j]= recurse(i+1,j,m,n,dp)+recurse(i,j+1,m,n,dp);
+        return dp[i][j];
+    }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> grid(m,vector<int>(n,0));
+        grid[m-1][n-1]=1;
+        return recurse(0,0,m,n,grid);
     }
 };
