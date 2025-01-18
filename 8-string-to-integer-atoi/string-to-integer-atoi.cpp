4585603effe0dbf23f8d0b3f45c042cce5b57a5c
Rohith Kumar <pathinarohith@gmail.com>
2025-01-18 09:41:31 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (80.34%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,18 +1,29 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        int i = 0, sign = 1, n = s.length();
-        for ( ;i < n && s[i] == ' '; i++) // Skip leading whitespace
-            ;
-        if (i < n && (s[i] == '+' || s[i] == '-')) { // Check for optional sign
-            sign = (s[i++] == '-') ? -1 : 1;
+        int i = 0, n = s.length();
+        // Skip leading whitespace
+        while (i < n && s[i] == ' ') i++;
+        
+        // Handle sign
+        bool isNeg = false;
+        if (i < n && (s[i] == '+' || s[i] == '-')) {
+            isNeg = (s[i] == '-');
+            i++;
         }
-        long num = 0;
-        while (i < n && isdigit(s[i]) && num <= INT_MAX) { // Convert digits to number
-            num = num * 10 + (s[i++] - '0');
+        
+        // Convert digits
+        int ans = 0;
+        while (i < n && s[i] >= '0' && s[i] <= '9') {
+            int digit = s[i] - '0';
+            // Check for overflow
+            if (ans > (INT_MAX - digit) / 10) {
+                return isNeg ? INT_MIN : INT_MAX;
+            }
+            ans = ans * 10 + digit;
+            i++;
         }
-        num = std::max(sign * num, (long) INT_MIN);
-        num = std::min(num, (long) INT_MAX);
-        return (int) num;
+        
+        return isNeg ? -ans : ans;
     }
 };
