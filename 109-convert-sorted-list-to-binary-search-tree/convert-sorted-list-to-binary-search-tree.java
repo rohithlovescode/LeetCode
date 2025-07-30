61cb9f0c58376e3caf9f95ad06adfe09f36c43cc
Rohith Kumar <pathinarohith@gmail.com>
2025-07-30 22:42:11 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (62.46%) - LeetSync

---
+++ 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.java
@@ -0,0 +1,17 @@
+class Solution {
+    public TreeNode sortedListToBST(ListNode head) {
+        if(head==null) return null;
+        if(head.next==null) return new TreeNode(head.val);
+        ListNode slow=head,fast=head,slow_Prev=null;
+        while(fast!=null && fast.next!=null){
+            slow_Prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        TreeNode root = new TreeNode(slow.val);//Making A Root Node
+        slow_Prev.next = null;//Braeking The Link For L1
+        root.left = sortedListToBST(head);
+        root.right = sortedListToBST(slow.next);
+        return root;
+    }
+}
