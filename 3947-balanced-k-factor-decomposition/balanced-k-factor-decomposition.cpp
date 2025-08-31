22c68652b5e160e22e1993a552ed97183ae8f906
Rohith Kumar <pathinarohith@gmail.com>
2025-08-31 12:15:55 +0530

    Time: 155 ms (24.24%) | Memory: 10.9 MB (46.48%) - LeetSync

---
+++ 3947-balanced-k-factor-decomposition/balanced-k-factor-decomposition.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    void solve(vector<int>& div, int ind, vector<int>& ans, vector<int>& temp,int prod,int n,int k,int mini, int maxi,int currAns){
+        if(ind>=div.size()|| prod>n){
+            return;
+        }
+        if(k==0){
+            if(prod==n&&(maxi-mini)<currAns){
+                ans=temp;
+                currAns=(maxi-mini);
+            }
+            return;
+        }
+        temp.push_back(div[ind]);
+        
+if (prod <= n / div[ind]) {  // prevents overflow and ensures prod*div[ind] <= n
+    solve(div, ind, ans, temp, prod * div[ind], n, k - 1,
+          min(mini, div[ind]), max(maxi, div[ind]), currAns);
+}
+
+        temp.pop_back();
+        solve(div,ind+1,ans,temp,prod,n,k,mini,maxi,currAns);
+    }
+    vector<int> minDifference(int n, int k) {
+        vector<int> divisors;
+        vector<int> ans;
+        vector<int> temp;
+        for(int i=1;i<=n;i++){
+            if(n%i==0){
+                divisors.push_back(i);
+            }
+        }
+        
+        solve(divisors,0,ans,temp,1,n,k,INT_MAX,INT_MIN,INT_MAX);
+        return ans;
+    }
+};
