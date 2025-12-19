b08d9f9050978f6a6271018d008b6c59ecca30c0
Rohith Kumar <pathinarohith@gmail.com>
2025-12-19 22:43:58 +0530

    Time: 7 ms (48.72%) | Memory: 20.1 MB (36.87%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -2,36 +2,37 @@ class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<int> indegree(numCourses,0);
-        vector<vector<int>> adjList(numCourses);
 
-        // Build graph with correct edge direction
-        for (const vector<int>& prereq : prerequisites) {
-            int course = prereq[0];
-            int pre = prereq[1];
-            adjList[pre].push_back(course); // pre -> course
-            indegree[course]++;             // increment course's indegree
+        vector<vector<int>> adjList(numCourses,vector<int>());//syntax
+
+        for(vector<int> prereq:prerequisites){//syn
+            int i=prereq[0];
+            int j=prereq[1];
+            adjList[j].push_back(i);
+            indegree[i]++;
         }
-
         queue<int> q;
-        for(int i = 0; i < numCourses; i++){
-            if(indegree[i] == 0) q.push(i);
-        }
-
-        int coursesComplete = 0;
-
-        while(!q.empty()){
-            int front = q.front();
-            q.pop();
-            coursesComplete++;
-
-            for(auto neigh : adjList[front]){
-                indegree[neigh]--;
-                if(indegree[neigh] == 0){
-                    q.push(neigh);
-                }
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0){
+                q.push(i);
             }
         }
 
-        return coursesComplete == numCourses;
+        
+        int coursesComplete=0;
+
+        while(!q.empty()){
+            int front=q.front();
+            q.pop();
+            coursesComplete++;
+            for(auto neigh:adjList[front]){
+                indegree[neigh]--;
+                if(indegree[neigh]==0){
+                    q.push(neigh);
+                }
+                
+            }
+        }
+        return coursesComplete==numCourses;
     }
 };
