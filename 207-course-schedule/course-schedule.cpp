80c80052690623fe6d0c37d45372fd8870b59f93
Rohith Kumar <pathinarohith@gmail.com>
2025-12-19 22:40:42 +0530

    Time: 1 ms (84.83%) | Memory: 19.2 MB (69.89%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -2,37 +2,36 @@ class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<int> indegree(numCourses,0);
+        vector<vector<int>> adjList(numCourses);
 
-        vector<vector<int>> adjList(numCourses,vector<int>());//syntax
-
-        for(vector<int> prereq:prerequisites){//syn
-            int i=prereq[0];
-            int j=prereq[1];
-            adjList[i].push_back(j);
-            indegree[j]++;
+        // Build graph with correct edge direction
+        for (const vector<int>& prereq : prerequisites) {
+            int course = prereq[0];
+            int pre = prereq[1];
+            adjList[pre].push_back(course); // pre -> course
+            indegree[course]++;             // increment course's indegree
         }
+
         queue<int> q;
-        for(int i=0;i<numCourses;i++){
-            if(indegree[i]==0){
-                q.push(i);
-            }
+        for(int i = 0; i < numCourses; i++){
+            if(indegree[i] == 0) q.push(i);
         }
 
-        
-        int coursesComplete=0;
+        int coursesComplete = 0;
 
         while(!q.empty()){
-            int front=q.front();
+            int front = q.front();
             q.pop();
             coursesComplete++;
-            for(auto neigh:adjList[front]){
+
+            for(auto neigh : adjList[front]){
                 indegree[neigh]--;
-                if(indegree[neigh]==0){
+                if(indegree[neigh] == 0){
                     q.push(neigh);
                 }
-                
             }
         }
-        return coursesComplete==numCourses;
+
+        return coursesComplete == numCourses;
     }
 };
