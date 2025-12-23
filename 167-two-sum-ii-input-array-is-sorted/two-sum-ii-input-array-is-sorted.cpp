77d4951d076a4d3b6cafe88257db6cf739c47c2c
Rohith Kumar <pathinarohith@gmail.com>
2025-12-23 09:21:49 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (30.52%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -5,7 +5,7 @@ public:
 
         while(left<right){
             if(target==(numbers[left]+numbers[right])){
-                return vector({left+1,right+1});
+                return {left+1,right+1};
             }else if(target>(numbers[left]+numbers[right])){
                 left++;
             }else{
