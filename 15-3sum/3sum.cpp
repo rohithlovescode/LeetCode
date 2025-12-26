0c83317a9b37dd6b922697c09c6943395cd228ca
Rohith Kumar <pathinarohith@gmail.com>
2025-12-27 00:55:31 +0530

    Time: 59 ms (25.63%) | Memory: 28.9 MB (89.48%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,37 +1,44 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> ans;
         sort(nums.begin(),nums.end());
         int n=nums.size();
-        vector<vector<int>> sol;
 
-        for(int i=0;i<n-2;i++){
+        for(int i=0;i<n;i++){
+            if(i>0&&nums[i-1]==nums[i]){
+                continue;
+            }
+            int left=i+1;
+            int right=n-1;
 
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-            int target=-nums[i];
-            int left=i+1,right=n-1;
             while(left<right){
-                if(left>i+1&&nums[left]==nums[left-1]){
-                    left++;
-                    continue;
-                }
-                if(right!=n-1&&nums[right]==nums[right+1]){
-                    right--;
-                    continue;
-                }
-                if(nums[left]+nums[right]==target){
-                    sol.push_back({nums[i],nums[left],nums[right]});
+                if(nums[i]+nums[left]+nums[right]==0){
+                    ans.push_back({nums[i],nums[left],nums[right]});//vector
                     left++;
                     right--;
+                    while(left<n&&nums[left]==nums[left-1]){
+                        left++;
+                    }
+                    while(right>=0&&right<n-1&&nums[right]==nums[right+1]){
+                        right--;
+                    }
                 }
-                else if(nums[left]+nums[right]<target){
-                    left++;
-                }
-                else{
+                else if(nums[i]+nums[left]+nums[right]>0){
                     right--;
+                    while(right>=0&&right<n-1&&nums[right]==nums[right+1]){
+                        right--;
+                    }
+                }else{
+                    left++;
+                    while(left<n&&nums[left]==nums[left-1]){
+                        left++;
+                    }
+
                 }
+
             }
         }
-        return sol;
+        return ans;
     }
 };
