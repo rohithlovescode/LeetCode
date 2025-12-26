326e450d4923bbc963d783b876b82e7880dc8114
Rohith Kumar <pathinarohith@gmail.com>
2025-12-26 23:48:10 +0530

    Time: 105 ms (18.03%) | Memory: 88.9 MB (60.07%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,59 +1,38 @@
 class Solution {
 public:
-    int solve(int num,vector<int>& nums,map<int,int>& available,int& ans){
-        if(available[num+1]==0){
-            available[num]=1;
-        }else{
-            if(available[num+1]==INT_MIN){
-                available[num]=solve(num+1,nums,available,ans)+1;
-            }else{
-                available[num]=available[num+1]+1;
-            }
-        }
-        ans=max(ans,available[num]);
-        return available[num];
-    }
     int longestConsecutive(vector<int>& nums) {
         
-        map<int,int> available;
-        for( auto num: nums){
-            available[num]=INT_MIN;
-        }
+        // [0 3 2 1]
+
+        // map
+        // 0: 4
+        // 3: 4
+        // 2: 2
+        // 1: 1+(1)+(2)=4
+        unordered_map<int,int> boundaryCheck;
         int ans=0;
-        for(int i=0;i<nums.size();i++)
-            solve(nums[i],nums,available,ans);
+        for(auto num: nums){
+            if(boundaryCheck.count(num)!=0){
+                continue;
+            }
+            
+            int leftConsecutive=0;
+            int rightConsecutive=0;
+            if(boundaryCheck.count(num-1)!=0){
+                leftConsecutive=boundaryCheck[num-1];
+            }
+            if(boundaryCheck.count(num+1)!=0){
+                rightConsecutive=boundaryCheck[num+1];
+            }
+            boundaryCheck[num]=leftConsecutive+rightConsecutive+1;
+            boundaryCheck[num-leftConsecutive]=boundaryCheck[num];
+            boundaryCheck[num+rightConsecutive]=boundaryCheck[num];
+            ans=max(ans,boundaryCheck[num]);
+
+
+
+
+        }
         return ans;
     }
 };
-
-// class Solution {
-// public:
-//     int longestConsecutive(vector<int>& nums) {
-//         if(nums.size()==0){
-//             return 0;
-//         }
-//         priority_queue<int> values;
-
-//         for(int i=0;i<nums.size();i++){
-//             values.push(nums[i]);
-//         }
-//         int temp=1,sol=1;
-//         int curr=values.top();
-//         values.pop();
-//         while(!values.empty()){
-//             if(values.top()==curr-1){
-//                 curr-=1;
-//                 temp++;
-//             }else if(values.top()==curr){
-                
-//             }
-//             else{
-//                 curr=values.top();
-//                 temp=1;
-//             }
-//             values.pop();
-//             sol=max(temp,sol);
-//         }
-//         return sol;
-//     }
-// };
