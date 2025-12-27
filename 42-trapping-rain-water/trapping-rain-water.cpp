d6cbddd574432d31997b9bd8bc7d19336afc5536
Rohith Kumar <pathinarohith@gmail.com>
2025-12-27 10:06:28 +0530

    Time: 0 ms (100.00%) | Memory: 26.1 MB (58.04%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -5,7 +5,7 @@ public:
         int leftMax = 0, rightMax = 0;
         int ans = 0;
 
-        while (left < right) {
+        while (left <= right) {
             if (height[left] <= height[right]) {
                 leftMax = max(leftMax, height[left]);
                 ans += leftMax - height[left];
