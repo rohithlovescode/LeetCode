82e90d88be79c920c09f12cd9acccce3965915ff
Rohith Kumar <pathinarohith@gmail.com>
2025-12-27 17:00:27 +0530

    Time: 27 ms (9.09%) | Memory: 23.2 MB (5.41%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,43 +1,40 @@
 class Solution {
 public:
-    char findMostRepeated(const map<char,int> &counts){
-        char mostRepeated='A';
-        int val=0;
-        for(auto count: counts){
-            if(count.second>val){
-                mostRepeated=count.first;
-                val=count.second;
-            }
-        }
-        return mostRepeated;
-    }
-    int characterReplacement(string s, int k) {
+    int calculateMaxSubstring(char ch,string s,int k){
+        int maxLength=0;
         int left=0,right=0;
-        int ans=1;
-        int leng=s.length();
-        char mostRepeated='A';
+        int changesLeft=k;
 
-        map<char,int> mapp;
-        map<int,bool> visited;
-        while(right<leng){
-            char currChar=s[right];
-            if(!visited[right]){
-                mapp[currChar]=mapp[currChar]+1;
-                visited[right]=true;
-            }
-            if(mapp[currChar]>mapp[mostRepeated]){
-                mostRepeated=currChar;
-            }
-            int temp=right-left+1-mapp[mostRepeated];
-            if(temp<=k){
-                ans=max(right-left+1,ans);
+        //"BBBBBAAAAABBBAAAA",k=5 changes
+        //  .........|
+        // cl=0
+        while(right<s.size()){
+            if(ch==s[right]){
                 right++;
             }else{
-                mapp[s[left]]--;
-                left++;
-                mostRepeated=findMostRepeated(mapp);
+                changesLeft--;
+                    right++;
+                while(changesLeft<0){
+                    if(ch!=s[left]){
+                        changesLeft++;
+                    }
+                    left++;
+                }
+                
             }
+            maxLength=max(maxLength,right-left);
         }
+        return maxLength;
+        
+    }
+    int characterReplacement(string s, int k) {
+        int ans=0;
+        
+        for(char ch='A';ch<='Z';ch++){
+            int maxSubstring=calculateMaxSubstring(ch,s,k);
+            ans=max(ans,maxSubstring);
+        }
+
         return ans;
     }
 };
