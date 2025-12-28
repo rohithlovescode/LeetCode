a1d4543a51a9aa0436df91e90dd1f6acc15ccf4c
Rohith Kumar <pathinarohith@gmail.com>
2025-12-28 15:23:31 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (65.54%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
+    bool checkParanthesis(char ch1,char ch2){
+        return ch1=='('&&ch2==')'||ch1=='{'&&ch2=='}'||ch1=='['&&ch2==']';
+    }
     bool isValid(string s) {
-        //we can use closingParentheses hashmap where for every '(' it is ), and so on
-        stack<int> parentheses;
-        for(char c: s){
-            if(c=='('||c=='{'||c=='['){
-                parentheses.push(c);
+        stack<char> st;
+        for(auto& ch: s){
+            if(ch=='('||ch=='{'||ch=='['){
+                st.push(ch);
             }
             else{
-                if(parentheses.empty()){
+                if(st.empty()){
                     return false;
                 }
-                if((c==')'&&parentheses.top()!='(')||(c==']'&&
-                    parentheses.top()!='[')||(c=='}'&&parentheses.top()!='{')){
+                if(checkParanthesis(st.top(),ch)){
+                    st.pop();
+                }else{
                     return false;
                 }
-                parentheses.pop();
             }
         }
-        if(parentheses.empty()){
-            return true;
-        }
-        return false;
+        return st.empty();
     }
 };
