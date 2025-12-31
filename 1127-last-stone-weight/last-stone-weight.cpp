089578832566bf5aac9b65e233a8810b6672320c
Rohith Kumar <pathinarohith@gmail.com>
2025-12-31 23:36:37 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (76.88%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.cpp
+++ 1127-last-stone-weight/last-stone-weight.cpp
@@ -2,24 +2,19 @@ class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
         priority_queue<int> pq;
+
         for(auto stone: stones){
             pq.push(stone);
         }
         while(pq.size()>1){
-            int top1=pq.top();
-            pq.pop();
-            int top2=pq.top();
-            pq.pop();
-            if(top1==top2){
-                continue;
-            }else{
-                top1=top1-top2;
-                pq.push(top1);
+            int maxi1=pq.top();pq.pop();
+            int maxi2=pq.top();pq.pop();
+            if(maxi1-maxi2>0){
+                pq.push(maxi1-maxi2);
             }
         }
-        if(pq.empty()){
-            return 0;
-        }
+
+        if(pq.empty()) return 0;
         return pq.top();
     }
 };
