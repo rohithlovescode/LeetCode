5c14dfd26f5b7b212236b639d42c63f7a6803fa9
Rohith Kumar <pathinarohith@gmail.com>
2025-12-31 10:17:59 +0530

    Time: 23 ms (53.29%) | Memory: 23.4 MB (64.21%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -1,15 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
 class Solution {
 public:
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        while (root) {
-            if (p->val > root->val && q->val > root->val) {
-                root = root->right;
-            } else if (p->val < root->val && q->val < root->val) {
-                root = root->left;
-            } else {
-                return root;
-            }
+    bool recurse(TreeNode* root, TreeNode*p, TreeNode* q, TreeNode* &ans){
+        if(root==nullptr) return false;
+        bool left=recurse(root->left, p,q,ans);
+        bool right=recurse(root->right,p,q,ans);
+
+        if((root==p||root==q)&&(left||right)){
+            cout<<root->val;
+
+            ans= root;
         }
-        return nullptr;        
+        else if(left&&right){
+            cout<<root->val;
+            ans=root;
+        }
+
+        return root==p||root==q||left||right;
+    }
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        TreeNode* ans=nullptr;
+        recurse(root,p,q,ans);
+        return ans;
     }
 };
