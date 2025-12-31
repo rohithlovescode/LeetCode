ca156716a7900e8ff6b3333017ba8e0b45eea910
Rohith Kumar <pathinarohith@gmail.com>
2025-12-31 16:23:33 +0530

    Time: 0 ms (100.00%) | Memory: 22 MB (45.21%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
-    bool validateBST(TreeNode* root, long long mini, long long maxi) {
-        if (root == nullptr) {
-            return true;
-        }
-        if (root->val <= mini || root->val >= maxi) {
-            return false;
-        }
-        return validateBST(root->left, mini, root->val) && validateBST(root->right, root->val, maxi);
-    }
+    bool dfs(TreeNode* root,long mini,long maxi){
+        if(root==NULL) return true;
+        if(root->val<=mini||root->val>=maxi) return false;
 
+        return dfs(root->left,mini,root->val)&&dfs(root->right,root->val,maxi);
+    }
     bool isValidBST(TreeNode* root) {
-        return validateBST(root, LLONG_MIN, LLONG_MAX);  // Use long long limits
+        return dfs(root,LONG_MIN,LONG_MAX);
     }
 };
