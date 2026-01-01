a10970438079839e3852161d1dc6f495fdbb2b78
Rohith Kumar <pathinarohith@gmail.com>
2026-01-01 22:41:19 +0530

    Time: 462 ms (5.00%) | Memory: 25.8 MB (5.13%) - LeetSync

---
+++ 135-candy/candy.cpp
@@ -0,0 +1,40 @@
+class Compare{
+    public:
+    bool operator()(const pair<int,int>& a,const pair<int,int>& b){
+        return a.first>b.first;
+    }
+};
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        priority_queue<pair<int,int>, vector<pair<int,int>>,Compare> pq;
+        int n=ratings.size();
+        int ans=0;
+
+        vector<int> candies(ratings.size(),0);
+        for(int i=0;i<n;i++){
+            pq.push({ratings[i],i});
+        }
+        while(!pq.empty()){
+            auto [val,ind]=pq.top(); pq.pop();
+            int minCandies=1;
+            if(ind>0){
+                if(ratings[ind-1]!=ratings[ind]){
+                    minCandies=max(minCandies,candies[ind-1]+1);
+                }
+            }
+            if(ind<n-1){
+                if(ratings[ind+1]!=ratings[ind]){
+                    minCandies=max(minCandies,candies[ind+1]+1);
+                }
+            }
+            ans+=minCandies;
+            candies[ind]=minCandies;
+            cout<<minCandies<<" "<<ind<<endl;
+
+        }
+
+        return ans;
+
+    }
+};
