c914739d9cc724e8c811111f8d6ce5034cd32fca
Rohith Kumar <pathinarohith@gmail.com>
2026-01-18 14:38:05 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (62.00%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
-    void solve(vector<int>& candidates,int ind,int target,vector<int>& temp,vector<vector<int>>& sol){
+    void backtrack(vector<int>& candidates,int ind,int target, vector<vector<int>>& ans,vector<int>& curr){
+        
         if(target==0){
-            sol.push_back(temp);
+            ans.push_back(curr);
             return;
         }
-        if(ind>=candidates.size()||target<0){
-            return;
-        }
-        temp.push_back(candidates[ind]);
-        solve(candidates,ind+1,target-candidates[ind],temp,sol);
-        temp.pop_back();
-        while (ind + 1 < candidates.size() && candidates[ind] == candidates[ind + 1]) {
+        if(ind>=candidates.size()||target-candidates[ind]<0) return;
+
+        curr.push_back(candidates[ind]);
+        backtrack(candidates,ind+1,target-candidates[ind],ans,curr);
+        curr.pop_back();
+
+        while(ind+1<candidates.size()&&candidates[ind+1]==candidates[ind]){
             ind++;
         }
-        solve(candidates,ind+1,target,temp,sol);
-
+        backtrack(candidates,ind+1,target,ans,curr);
     }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        vector<vector<int>> sol;
-        vector<int> temp;
         sort(candidates.begin(),candidates.end());
-        solve(candidates,0,target,temp,sol);
-        return sol;
+        // backtrack
+        vector<vector<int>> ans;
+        vector<int> curr;
+        backtrack(candidates,0,target,ans,curr);
+
+
+        return ans;
     }
 };
