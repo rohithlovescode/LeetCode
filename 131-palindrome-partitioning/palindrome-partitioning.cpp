a8103a90dd8a4d0c485700383300ba2825b5d0ab
Rohith Kumar <pathinarohith@gmail.com>
2026-01-19 20:52:29 +0530

    Time: 30 ms (89.27%) | Memory: 53.2 MB (98.38%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,34 +1,35 @@
 class Solution {
-private:
-    void sub(int ind, string s, vector<string>& temp, vector<vector<string>>& res) {
-        if (ind == s.size()) {
-            res.push_back(temp);
+public:
+    bool isPalindrome(string str){
+        int leng=str.length();
+        for(int i=0;i<leng/2;i++){
+            if(str[i]!=str[leng-i-1]) return false;
+        }
+
+        return true;
+    }
+    void recurse(int ind, vector<vector<string>>& ans,vector<string> &partitioning,string &s){
+        if(ind>=s.length()) {
+            ans.push_back(partitioning);
             return;
         }
-        for (int i = ind; i < s.size(); i++) {
-            if (ispalindrome(ind, i, s)) {
-                temp.push_back(s.substr(ind, i - ind + 1));
-                sub(i + 1, s, temp, res);
-                temp.pop_back();  // backtrack
+        string str="";
+
+        for(int i=ind;i<s.length();i++){
+            str+=s[i];
+            if(isPalindrome(str)) {
+                partitioning.push_back(str);
+                recurse(i+1,ans,partitioning,s);
+                partitioning.pop_back();
             }
         }
     }
-
-    bool ispalindrome(int start, int end, string s) {
-        while (start <= end) {
-            if (s[start] != s[end])
-                return false;
-            start++;
-            end--;
-        }
-        return true;
-    }
-
-public:
     vector<vector<string>> partition(string s) {
-        vector<string> temp;
-        vector<vector<string>> res;
-        sub(0, s, temp, res);
-        return res;
+        vector<vector<string>> ans;
+        vector<string> partitioning;
+
+        recurse(0,ans,partitioning,s);
+        return ans;
+
     }
 };
