8b3db39796c829e11405f20ab7d23c7af9c21d78
Rohith Kumar <pathinarohith@gmail.com>
2026-01-19 17:25:16 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (89.04%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
-    void solve(vector<string>& sol,string currString,int numOpen,int numClosed){
-        if(numOpen==0&&numClosed==0){
-            sol.push_back(currString);
-            return;
+    void generate(int n,string& curr, int offset,vector<string>& ans){
+        if(n==0&&offset==0){
+            ans.push_back(curr);
         }
-        else if(numOpen<0||numClosed<numOpen){
-            return;
+
+        if(n>0){
+            curr+='(';
+            generate(n-1,curr,offset+1,ans);
+            curr.pop_back();
         }
-        solve(sol,currString+'(',numOpen-1,numClosed);
-        solve(sol,currString+')',numOpen,numClosed-1);
+        if(offset>0){
+            curr+=')';
+            generate(n,curr,offset-1,ans);
+            curr.pop_back();
+        }
     }
     vector<string> generateParenthesis(int n) {
-        vector<string> sol;
-        solve(sol,"",n,n);
-        return sol;
+        vector<string> ans;
+
+        int offset=0;//determines (openBrackets-closedBrackets)
+
+        string curr="";
+
+        generate(n,curr,offset,ans);
+
+        return ans;
+
+
     }
 };
