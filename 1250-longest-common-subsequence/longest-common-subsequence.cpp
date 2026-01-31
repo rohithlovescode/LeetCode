9c895c82b456d5c825f354b44deacefc77121ba8
Rohith Kumar <pathinarohith@gmail.com>
2026-01-31 11:48:44 +0530

    Time: 51 ms (6.84%) | Memory: 27.5 MB (52.01%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,32 +1,22 @@
 class Solution {
-    int solve(string& text1 , string& text2 , vector<vector<int>>& dp,int ind1,int ind2){
-        if(ind1==-1||ind2==-1){
-            return 0;
-        }
-        if(dp[ind1][ind2]!=-1){
-            return dp[ind1][ind2];
-        }
-        else if(text1[ind1]==text2[ind2]){
-            dp[ind1][ind2]= 1+solve(text1,text2,dp,ind1-1,ind2-1);
-            
-            return dp[ind1][ind2];
-        }
-        else{//text1[leng1-1]!=text2[leng2-1]
-            int left=solve(text1,text2,dp,ind1-1,ind2);
-            int right=solve(text1,text2,dp,ind1,ind2-1);
-            dp[ind1][ind2]=max(left,right);
-            
-            return dp[ind1][ind2];
-        }
-        
-
-    }
 public:
-    int longestCommonSubsequence(string text1, string text2) {
-        int ind1=text1.length()-1;
-        int ind2=text2.length()-1;
+    int topDown(string& text1, string& text2, int ind1,int ind2,vector<vector<int>>& dp){
+        if(ind1>=text1.size()||ind2>=text2.size()) return 0;
 
-        vector<vector<int>> dp(ind1+1,vector<int> (ind2+1,-1));
-        return solve(text1,text2,dp,ind1,ind2);
+        if(dp[ind1][ind2]!=-1) return dp[ind1][ind2];
+
+        if(text1[ind1]==text2[ind2]) dp[ind1][ind2] = 1+topDown(text1,text2,ind1+1,ind2+1,dp);
+        else dp[ind1][ind2] = max(topDown(text1,text2,ind1+1,ind2,dp),
+                                    topDown(text1,text2,ind1,ind2+1,dp));
+        return dp[ind1][ind2];
+    }
+    int longestCommonSubsequence(string text1, string text2) {
+        int l1=text1.length();
+        int l2=text2.length();
+
+        vector<vector<int>> dp(l1,vector<int>(l2,-1));
+
+        return topDown(text1,text2,0,0,dp);//function names, hwo to determine
+        
     }
 };
