f8b87d1ceb3d2af69aca865367cd2a66e605a14a
Rohith Kumar <pathinarohith@gmail.com>
2026-02-01 12:47:37 +0530

    Time: 157 ms (33.41%) | Memory: 60.4 MB (9.26%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
-    int solve(vector<int>& nums, int target, int ind, map<pair<int,int>,int>& ways){
-        if(ind==nums.size()){
-            return target==0?1:0;
+    int solve(vector<int>& nums, int target, int ind, vector<unordered_map<int, int>>& ways) {
+        if (ind == nums.size()) {
+            return target == 0 ? 1 : 0;
         }
-        if(ways.count({ind,target})!=0) return ways[{ind,target}];
-        ways[{ind,target}]=solve(nums,target-nums[ind],ind+1,ways)+solve(nums,target+nums[ind],ind+1,ways);
-        return ways[{ind,target}];
-    }
-    int findTargetSumWays(vector<int>& nums, int target) {
-        map<pair<int,int>,int> ways;
-        int n=nums.size();
+        // Check map at specific index
+        if (ways[ind].count(target)) return ways[ind][target];
 
-        return solve(nums, target,0,ways);
+        ways[ind][target] = solve(nums, target - nums[ind], ind + 1, ways) + 
+                            solve(nums, target + nums[ind], ind + 1, ways);
+        return ways[ind][target];
+    }
+
+    int findTargetSumWays(vector<int>& nums, int target) {
+        // Create a vector where each index holds its own map
+        vector<unordered_map<int, int>> ways(nums.size());
+        return solve(nums, target, 0, ways);
     }
 };
