bceaa9394682588fcbeea0d002de74aa9ce3df9b
Rohith Kumar <pathinarohith@gmail.com>
2026-02-05 19:01:56 +0530

    Time: 0 ms (100.00%) | Memory: 25.9 MB (8.08%) - LeetSync

--- 3651-transformed-array/transformed-array.cpp
+++ 3651-transformed-array/transformed-array.cpp
@@ -4,7 +4,7 @@ public:
         vector<int> ans(nums.size());
         int n=nums.size();
         for(int i=0;i<nums.size();i++){
-            int ind=(i+(nums[i])%n+n)%n;
+            int ind=(i+nums[i]+100*n)%n;
             ans[i]=nums[ind];
         }
         return ans;
