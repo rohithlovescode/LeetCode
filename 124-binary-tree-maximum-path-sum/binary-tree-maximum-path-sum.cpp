4f635b439d61bd0638329d7860d0781e27458c32
Rohith Kumar <pathinarohith@gmail.com>
2026-04-14 01:11:40 +0530

    Time: 0 ms (100.00%) | Memory: 27.9 MB (45.74%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
@@ -1,21 +1,39 @@
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int dfs(TreeNode* root,int& ans){
-        if(!root) return 0;
-        int left=dfs(root->left,ans);
-        int right=dfs(root->right,ans);
+    int solve(TreeNode* node, int & maxSum){
+        if(node==nullptr) return 0;
+
+        int left = solve(node->left,maxSum);
+        int right = solve(node->right,maxSum);
+
         
-        left=left>0?left:0;
-        right=right>0?right:0;
-        ans=max(ans,root->val+left+right);
-        
-        return max(left,right)+root->val;
-        
+        int currSum=node->val;
+        if(left>0) currSum+=left;
+        if(right>0) currSum+=right;
+
+        maxSum=max(currSum,maxSum);
+
+
+        int returnVal=node->val+max(0,max(left,right));
+        return returnVal;
+
     }
     int maxPathSum(TreeNode* root) {
-        int ans=INT_MIN;
-        dfs(root,ans);
-        return ans;
+        int maxSum=INT_MIN;
+
+        solve(root,maxSum);
+
+        return maxSum;
     }
 };
